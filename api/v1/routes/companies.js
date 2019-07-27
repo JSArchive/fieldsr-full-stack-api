e40da6ab6b3629d64366ae7a13484728f7c0c7aa
dcv436 <dcv436@dcv436s-MacBook-Pro.local>
2019-07-26 18:58:41 -0700

    go over todo and things to keep testing

--- api/v1/routes/companies.js
+++ api/v1/routes/companies.js
@@ -3,6 +3,15 @@ const Companies = require('../models/companies')
 
 router.get('/', (req, res, next) => {
     const status = 200
+
+//Notes / To-Do, test that these work with req.query or if I need more logiic:
+
+//  GET /api/v1/companies?employees_lte=[integer]
+
+// GET /api/v1/companies?employees_gte=[integer]
+
+
+
     
 // Return all Companies.  If query is passed evaulate query.
 router.get('/', (req, res, next) => {
--- api/v1/routes/employees.js
+++ api/v1/routes/employees.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 const Employees = require('../models/employees')
 
+
+//Notes / To-Do, test that these work with req.query or if I need more logiic:
+
+// GET /api/v1/employees?name=[partial-query]
+
+// GET /api/v1/employees?birthday=[date]
+
+
+
 // Return all Employees.  If query is passed evaulate query.
 router.get('/', (req, res, next) => {
     const status = 200
